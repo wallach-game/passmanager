c77fd101718dffe30acebe185205340e3564005e
Jiri Korenek <jirklym@gmail.com>
2024-02-26 21:56:12 +0100

    dsadas

--- createPassword.sh
+++ createPassword.sh
@@ -10,8 +10,18 @@ else
 fi
 
 
+testPassword="testPassword"
+testUsername="testUsername"
 
-sleep 10
+pin=$(termux-dialog text -t "Enter PIN:" -n -p )
+
+# Generate an encryption key using the passphrase
+encryption_key=$(echo -n "$pin" | openssl enc -aes-256-cbc -pass pass:"$passphrase" -nosalt -base64)
+encrypted_string=$(echo -n "$testPassword" | openssl enc -aes-256-cbc -pass "pass:$encryption_key" -nosalt -base64)
+
+echo "$encrypted_string" > "$testUsername"
+
+sleep 5
 ##
 ##pin=$(termux-dialog -t "Enter your PIN" -p -n )
 
