ef3064f87c1af3a700a74ec7030bac2bea96c981
Jiri Korenek <jirklym@gmail.com>
2024-02-26 21:42:01 +0100

    fsdfd

--- createPassword.sh
+++ createPassword.sh
@@ -1,6 +1,6 @@
 termux-toast -s "Saving a new password"
 dialog1=$(termux-dialog radio -v generate,type )
-option=echo "$dialog1" | jq -r ".index"
+option=$(echo "$dialog1" | jq -r ".index" )
 echo "$option"
 sleep 10
 ##
