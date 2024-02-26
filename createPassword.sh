46ce9f7d33687c9c7db6b216a21291bcf3476770
Jiri Korenek <jirklym@gmail.com>
2024-02-26 21:34:47 +0100

    test

--- createPassword.sh
+++ createPassword.sh
@@ -1,6 +1,7 @@
-termux-toast "Saving a new password"
+termux-toast -s "Saving a new password"
 option=$(termux-dialog radio -t "Choose option" -v generate, "type password" | jq -r ".index")
 echo $(option)
+sleep 10
 ##
 ##pin=$(termux-dialog -t "Enter your PIN" -p -n )
 
