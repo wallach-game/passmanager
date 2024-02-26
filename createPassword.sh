cc89b5c30d82903e1b8ab70c399607d3589fa0aa
Jiri Korenek <jirklym@gmail.com>
2024-02-26 21:33:29 +0100

    uadpte

--- createPassword.sh
+++ createPassword.sh
@@ -1,6 +1,7 @@
 termux-toast "Saving a new password"
-option=$(termux-dialog radio -t "Choose option" -v generate, "type password"
-| jq -r ".index")
+option=$(termux-dialog radio -t "Choose option" -v generate, "type password" | jq -r ".index")
+echo=$(option)
 ##
 ##pin=$(termux-dialog -t "Enter your PIN" -p -n )
+
 
