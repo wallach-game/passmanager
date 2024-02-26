899a3832f8de310ead7295d93ac27f115e4b8638
Jiri Korenek <jirklym@gmail.com>
2024-02-26 21:33:58 +0100

    dasd

--- createPassword.sh
+++ createPassword.sh
@@ -1,6 +1,6 @@
 termux-toast "Saving a new password"
 option=$(termux-dialog radio -t "Choose option" -v generate, "type password" | jq -r ".index")
-echo=$(option)
+echo $(option)
 ##
 ##pin=$(termux-dialog -t "Enter your PIN" -p -n )
 
